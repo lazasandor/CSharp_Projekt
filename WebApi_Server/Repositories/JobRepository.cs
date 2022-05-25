aa93d0540717d456fa3950cc7934b974f131d3c7
DMartin20 <dobi.vv@gmail.com>
2022-05-25 16:30:37 +0200

    Database Methods

---
+++ WebApi_Server/Repositories/JobRepository.cs
@@ -0,0 +1,71 @@
+using System;
+using WebApi_Server.Models;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+
+namespace WebApi_Server.Repositories
+{
+    public static class JobRepository
+    {
+        public static IList<Job> GetJobs()
+        {
+            using (var database = new JobContext())
+            {
+                var jobs = database.Jobs.ToList();
+
+                return jobs;
+            }
+        }
+
+        public static Job GetJob(long id)
+        {
+            using (var database = new JobContext())
+            {
+                var jobid = database.Jobs.Where(j => j.Id == id).FirstOrDefault();
+
+                return jobid;
+            }
+        }
+
+        public static void AddJob(Job job)
+        {
+            using (var database = new JobContext())
+            {
+                database.Jobs.Add(job);
+
+                database.SaveChanges();
+            }
+
+        }
+
+        public static void UpdateJob(Job job)
+        {
+            using (var database = new JobContext())
+            {
+                
+               database.Jobs.Update(job);
+
+               database.SaveChanges();
+            }
+
+        }
+
+        public static void DeleteJob(Job job)
+        {
+            using (var database = new JobContext())
+            {
+                database.Jobs.Remove(job);
+
+                database.SaveChanges();
+            }
+        }
+
+
+
+    }
+
+}
